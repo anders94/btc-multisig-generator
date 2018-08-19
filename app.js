d9fc83c59662f35059b811b8fd11ab9c4c502729
Anders Brownworth <anders@Anderss-MacBook-Pro.local>
2018-08-19 07:12:23 -0400

    tighten up output

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ pubkeys.push(keyPair3.publicKey);
 const redeem = bitcoin.payments.p2ms({m: 2, pubkeys});
 const p2sh = bitcoin.payments.p2sh({redeem: redeem});
 
-console.log('address', p2sh.address, 'redeem script:', redeem.output.toString('hex'));
-showKey('  key 1', keyPair1);
-showKey('  key 2', keyPair2);
-showKey('  key 3', keyPair3);
+console.log(p2sh.address);
+console.log('redeem script:', redeem.output.toString('hex'));
+showKey('  key 1:', keyPair1);
+showKey('  key 2:', keyPair2);
+showKey('  key 3:', keyPair3);
 
 function showKey(txt, keyPair) {
-    const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
-    console.log(txt, address, 'public key in hex:', keyPair.publicKey.toString('hex'), 'private key in WIF:', keyPair.toWIF());
+    console.log(txt, 'private key in WIF:', keyPair.toWIF(), 'public key in hex:', keyPair.publicKey.toString('hex'));
 }
